59577c19828ee9c62ecd6cc8fe854bdb267eeee5
Eneroth3 <eneroth3@gmail.com>
2019-10-25 22:11:45 +0200

    Add option to fit content to full viewport, including gray bars

--- zoom.rb
+++ zoom.rb
@@ -14,6 +14,8 @@ module Zoom
   # @param view [Sketchup::View]
   # @param padding [Numeric] How many percent of view should be left blank on
   #   each side.
+  # @param full [Boolean] Fit content to full view, including gray bars if
+  #   aspect ratio is set.
   #
   # @example
   #   // Zoom extents
@@ -23,14 +25,14 @@ module Zoom
   #   Zoom.zoom_entities(Sketchup.active_model.selection)
   #
   #   // Zoom extents and adapt aspect ratio
-  #   View.reset_aspect_ratio
-  #   View.set_aspect_ratio(Zoom.zoom_entities(Sketchup.active_model.entities, padding: 0))
+  #   View.set_aspect_ratio(Zoom.zoom_entities(Sketchup.active_model.entities,
+  #                         padding: 0, full: true))
   #
   # @return [Float] Aspect ratio.
   def self.zoom_entities(entities, view = Sketchup.active_model.active_view,
-                         padding: 2.5)
+                         padding: 2.5, full: false)
     entities = [entities] unless entities.respond_to?(:each)
-    zoom_points(points(entities), view, padding: padding)
+    zoom_points(points(entities), view, padding: padding, full: full)
   end
 
   # Position camera to include points.
@@ -39,26 +41,28 @@ module Zoom
   # @param view [Sketchup::View]
   # @param padding [Numeric] How many percent of view should be left blank on
   #   each side.
+  # @param full [Boolean] Fit content to full view, including gray bars if
+  #   aspect ratio is set.
   #
   # @return [void]
   def self.zoom_points(points, view = Sketchup.active_model.active_view,
-                       padding: 2.5)
+                       padding: 2.5, full: false)
     raise ArgumentError, "Padding must be smaller than 50%" if padding >= 50
     return if points.empty?
 
     if view.camera.perspective?
-      zoom_perspective(points, view, padding)
+      zoom_perspective(points, view, padding, full)
     else
-      zoom_parallel(points, view, padding)
+      zoom_parallel(points, view, padding, full)
     end
   end
 
   # Private
 
-  def self.zoom_parallel(points, view, padding)
+  def self.zoom_parallel(points, view, padding, full)
     transformation = camera_space(view)
 
-    extremes = extreme_planes(points, view, padding)
+    extremes = extreme_planes(points, view, padding, full)
     extremes.map! { |pl| pl.map { |c| c.transform(transformation.inverse) } }
 
     # Honor padding. Even if included when creating the frustum it is lost
@@ -72,14 +76,15 @@ module Zoom
     ).transform(transformation)
 
     view.camera.set(eye, view.camera.direction, view.camera.up)
-    set_zoom(view, width, height)
+    set_zoom(view, width, height, full)
 
     width / height
   end
   private_class_method :zoom_parallel
 
-  def self.set_zoom(view, width, height)
-    if View.aspect_ratio(view) > width / height
+  def self.set_zoom(view, width, height, full)
+    aspect_ratio = full ? View.vp_aspect_ratio(view) : View.aspect_ratio(view)
+    if aspect_ratio > width / height
       View.set_height(height, view)
     else
       View.set_width(width, view)
@@ -87,10 +92,10 @@ module Zoom
   end
   private_class_method :set_zoom
 
-  def self.zoom_perspective(points, view, padding)
+  def self.zoom_perspective(points, view, padding, full)
     transformation = camera_space(view)
 
-    extremes = extreme_planes(points, view, padding)
+    extremes = extreme_planes(points, view, padding, full)
     extremes.map! { |pl| pl.map { |c| c.transform(transformation.inverse) } }
 
     line_y = Geom.intersect_plane_plane(extremes[0], extremes[1])
@@ -107,7 +112,8 @@ module Zoom
     bb = Geom::BoundingBox.new
     bb.add(points.map { |pt| view.screen_coords(pt) })
     width = [(bb.max.x - view.center.x).abs, (bb.min.x - view.center.x).abs].max
-    height = [(bb.max.y - view.center.y).abs, (bb.min.y - view.center.y).abs].max
+    height =
+      [(bb.max.y - view.center.y).abs, (bb.min.y - view.center.y).abs].max
 
     width / height
   end
@@ -145,6 +151,7 @@ module Zoom
   # @param points [Array<Geom::Point3d>]
   # @param view [Sketchup::View]
   # @param padding [Numeric]
+  # @param full [Boolean]
   #
   # @return [Array<(
   #   Array<(Geom::Point3d, Geom::Vector3d)>,
@@ -152,8 +159,8 @@ module Zoom
   #   Array<(Geom::Point3d, Geom::Vector3d)>,
   #   Array<(Geom::Point3d, Geom::Vector3d)>
   #   )]
-  def self.extreme_planes(points, view, padding)
-    Frustum.planes(view, padding: padding).map do |plane|
+  def self.extreme_planes(points, view, padding, full)
+    Frustum.planes(view, padding: padding, full: full).map do |plane|
       transformation = Geom::Transformation.new(*plane).inverse
       point = points.max_by { |pt| pt.transform(transformation).z }
 
